c40c179bcfa556a49be3bdef07fc7e3c83edf8dd
salama hamdy salama <www.elprof929@gmail.com>
2026-06-27 13:46:35 +0300

    fix base path to match repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/prime-medical1/', 
+  base: '/prime-medical-v2/', // التعديل الجديد بالاسم الصح للمستودع 
 })
